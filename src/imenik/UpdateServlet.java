3a2843ffe80a965f8377559a317984675c287b89
djomla79 <tm.djomla79@gmail.com>
2015-11-16 01:20:28 +0100

    Imenik nedovrsen, validacije jos ne sljakaju.

--- src/imenik/UpdateServlet.java
+++ src/imenik/UpdateServlet.java
@@ -1,9 +1,6 @@
 package imenik;
 
-
 import java.io.IOException;
-import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
